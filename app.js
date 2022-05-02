449b0a0bd93e6b63c22fa1848103c2891e32a179
Hays4Days <gerushays@gmail.com>
2022-05-02 15:15:13 -0600

    validation set

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const promptUser = () => {
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubUsername => {
+            if(githubUsername) {
+                return true;
+            } else {
+                console.log('Please enter your github username!');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
@@ -34,12 +50,28 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your project?'
+                message: 'What is the name of your project? (Required)',
+                validate: projName => {
+                    if(projName) {
+                        return true;
+                    } else {
+                        console.log('Please enter the project name!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required)'
+                message: 'Provide a description of the project (Required)',
+                validate: projDesc => {
+                    if(projDesc) {
+                        return true;
+                    } else {
+                        console.log('Please enter the project description!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'checkbox',
@@ -50,7 +82,15 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)'
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: projLink => {
+                    if(projLink) {
+                        return true;
+                    } else {
+                        console.log('Please enter the github link to your project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'confirm',
