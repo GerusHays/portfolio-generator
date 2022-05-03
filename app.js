c3455b2dae128fcc0ddeae26db493f1c2180f581
Hays4Days <gerushays@gmail.com>
2022-05-03 15:16:17 -0600

    html structure

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+
 const promptUser = () => {
     return inquirer.prompt([
     {
@@ -131,16 +134,11 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
-});
+    const pageHTML = generatePage(portfolioData);
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
